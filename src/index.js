426469ba5c4ee2a21e81d9920be84aee6c8401ec
崔同洋 <cuitongyang@chuangjiangx.com>
2018-11-15 05:38:00 +0800

    修改容器组件

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import {Provider} from 'react-redux'
 
 import './styles/index.less'
 
-import App from './containers/App'
+import App from './containers/App.jsx'
 
 import store from './redux/store'
 
